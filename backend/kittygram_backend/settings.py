3387bbce1fd3f05e8b3e1c56b7e3daa4446d9ffe
Rinat Khaibullin <rinatkhaibullin@MacBook-Pro-Rinat.local>
2023-07-05 21:22:45 +0900

    fix

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,10 +1,8 @@
 import os
 from pathlib import Path
-
 from dotenv import load_dotenv
 
-env_path = Path(__file__).resolve().parent.parent / '.env'
-load_dotenv(env_path)
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
